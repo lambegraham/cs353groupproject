97ac4dee58aa1f83c6226c9a299fe3fcc8482175
Graham Lambe <lambegraham@gmail.com>
2019-10-17 13:12:07 +0100

    installed rest

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
-var express = require('express'); //import express 
+var express = require('express');
 var app = express();
 
-//to handle HTTP get request
 app.get('/', function (req, res) {
-  console.log("Get Request");
-  res.send("Get Request");
+  console.log("HTTP Get Request");
+  res.send("HTTP GET Request");
 ***REMOVED***);
 
-//start server on port: 8080
+app.put('/', function (req, res) {
+  console.log("HTTP Put Request");
+  res.send("HTTP PUT Request");
+***REMOVED***);
+
+app.post('/', function (req, res) {
+  console.log("HTTP POST Request");
+  res.send("HTTP POST Request");  
+***REMOVED***);
+
+app.delete('/', function (req, res) {
+  console.log("HTTP DELETE Request");
+  res.send("HTTP DELETE Request");
+***REMOVED***);
+
 var server = app.listen(8080, function () {
 
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log("server listening at http://%s:%s", host, port);
+  console.log("Example app listening at http://%s:%s", host, port);
 ***REMOVED***);
+
 var firebase = require('firebase');
 
 var firebaseConfig = {
