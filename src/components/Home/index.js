07075b4b28de859665abc9cae53fd23496e3a68a
Jack <jack.kearney.2018@mumail.ie>
2019-11-26 21:23:06 +0000

    CRUD now loads on the home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CRUD from '../CRUD/react-firebase/src/CRUD.js'
+
 const Home = () => (
   <div class = "container">
     <h1>Home</h1>
     <body>
-   
+    <CRUD/>
     </body>    
   </div>
 );
