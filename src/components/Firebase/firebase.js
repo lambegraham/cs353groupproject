b8245a3f925e152fa581d0d31bb5fba60dee41af
Jack <jack.kearney.2018@mumail.ie>
2019-11-26 21:20:03 +0000

    Updated firebase settings

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,14 +1,25 @@
 ***REMOVED***
 ***REMOVED***
-
-
 ***REMOVED***
+
+/****REMOVED***
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+***REMOVED***;*/
+
+***REMOVED***
+  apiKey: "AIzaSyCLhR7zyHM99cWqOvPtPkUmE55Eyv_CyDE",
+  authDomain: "cs353groupproject-d4b21.firebaseapp.com",
+  databaseURL: "https://cs353groupproject-d4b21.firebaseio.com",
+  projectId: "cs353groupproject-d4b21",
+  storageBucket: "cs353groupproject-d4b21.appspot.com",
+  messagingSenderId: "209584389821",
+  appId: "1:209584389821:web:2c50e880a785f2cf3881d4",
+  measurementId: "G-60VNQQ1MB7"
 ***REMOVED***;
 
 
