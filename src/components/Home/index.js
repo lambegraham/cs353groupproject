4a6ae3eef51b2b1add2cb304245cf77739b909af
ECannon <ericcannon@outlook.com>
2019-12-04 15:49:51 +0000

    Working towards input to firebase, not fully working

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,105 @@
 import React from 'react';
-import CRUD from '../CRUD/CRUD.js'
-import { withAuthorization ***REMOVED*** from '../Session';
+import * as firebase from 'firebase';
 
-const Home = () => (
-  <div class = "container">
-    <h1>Home</h1>
-    <body>
-      {/* <CRUD/>  */***REMOVED***
-    </body>    
-  </div>
-);
-const condition = authUser => !!authUser;
+//THIS IS NOT WORKING FULLY***********************************************
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''***REMOVED***;
 
-export default withAuthorization(condition)(Home);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+***REMOVED***
+
+  handleChange(event) {
+    this.setState({value: event.target.value***REMOVED***);
+***REMOVED***
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+***REMOVED***
+
+
+    onCreate = () => { //creates data to the firebase
+    var user = firebase.auth().currentUser;
+    const uid = user.uid;
+    console.log(uid);
+    const db = firebase.firestore();
+    // db.collection('users').doc(uid).collection("Modules").add({moduleCode: ***REMOVED***)
+    //db.collection('users').add({moduleCode: newModuleCode, moduleName: newModuleName, moduleCA: newModuleCA, moduleExam: newModuleExam***REMOVED***);
+    // db.collection('users').doc(uid).collection("Modules").add({
+    //   moduleCode: newModuleCode, moduleName: newModuleName, moduleCA: newModuleCA, moduleExam: newModuleExam
+    // ***REMOVED***
+    // );
+***REMOVED***
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit***REMOVED***>
+        <label>
+          ModuleCode:
+          <input type="text" value={this.state.value***REMOVED*** onChange={this.handleChange***REMOVED*** />
+        </label>
+
+        <label>
+          ModuleName:
+          <input type="text" value={this.state.value***REMOVED*** onChange={this.handleChange***REMOVED*** />
+        </label>
+
+        <label>
+          ModuleCA:
+          <input type="text" value={this.state.value***REMOVED*** onChange={this.handleChange***REMOVED*** />
+        </label>
+
+        <label>
+          Module Exam:
+          <input type="text" value={this.state.value***REMOVED*** onChange={this.handleChange***REMOVED*** />
+        </label>
+
+        <input type="submit" value="Submit" />
+      </form>
+    );
+***REMOVED***
+***REMOVED***
+
+// ReactDOM.render(
+//   <NameForm />,
+//   document.getElementById('root')
+// );
+
+export default NameForm;
+
+
+
+
+
+
+//**********Below here is the older code, keep it here ************************/
+// import React from 'react';
+// import * as firebase from 'firebase';
+// import 'firebase/firestore'
+// import CRUD from '../CRUD/CRUD.js'
+// import { GradeInput ***REMOVED*** from '../CRUD/GradeInput';
+
+// import { withAuthorization ***REMOVED*** from '../Session';
+
+// export const Home = () => (
+
+
+//   <div class = "container">
+//     <h1>Home</h1>
+//     <body>
+//       {/* <CRUD/>  */***REMOVED***
+//     </body>    
+//   </div>
+
+
+
+// );
+
+
+// const condition = authUser => !!authUser;
+
+// // export default tData;
+// export default withAuthorization(condition)(Home);
