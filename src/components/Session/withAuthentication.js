4ac2fd388d5e3364df15400719f10f2b0da8cfc4
Jake McMahon <jakemcmahon98@gmail.com>
2019-12-12 13:49:10 +0000

    Layout and Design changes
    Several small layout and formatting changes as well as design on the carousel.

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -1,31 +1,38 @@
 import React from 'react';
 
 import AuthUserContext from './context';
-import { withFirebase ***REMOVED*** from '../Firebase';
+import {
+  withFirebase
+***REMOVED*** from '../Firebase';
 
 const withAuthentication = Component => {
-  class WithAuthentication extends React.Component {
-    constructor(props) {
-      super(props);
+    class WithAuthentication extends React.Component {
+        constructor(props) {
+          super(props);
 
-      this.state = {
-        authUser: null,
-    ***REMOVED***
-  ***REMOVED***
+          this.state = {
+            authUser: null,
+        ***REMOVED***
+      ***REMOVED***
 
-    componentDidMount() {
-      this.listener = this.props.firebase.auth.onAuthStateChanged(
-        authUser => {
-          authUser
-            ? this.setState({ authUser ***REMOVED***)
-            : this.setState({ authUser: null ***REMOVED***);
-      ***REMOVED***,
-      );
-  ***REMOVED***
+        componentDidMount() {
+          this.listener = this.props.firebase.auth.onAuthStateChanged(
+            authUser => {
+              authUser
+                ?
+                this.setState({
+                  authUser
+              ***REMOVED***) :
+                this.setState({
+                  authUser: null
+              ***REMOVED***);
+          ***REMOVED***,
+          );
+      ***REMOVED***
 
-    componentWillUnmount() {
-      this.listener();
-  ***REMOVED***
+        componentWillUnmount() {
+          this.listener();
+      ***REMOVED***
 
     render() {
       return (
@@ -35,8 +42,7 @@ const withAuthentication = Component => {
       );
   ***REMOVED***
 ***REMOVED***
-
-  return withFirebase(WithAuthentication);
+return withFirebase(WithAuthentication);
 ***REMOVED***;
 
 export default withAuthentication;
